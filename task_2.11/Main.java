1c49cb5df090bf3d871f6e6e6a0b607b9757b86d
Helen <64192579+aloe99@users.noreply.github.com>
2020-04-27 23:31:04 +0300

    Create Main.java

---
+++ task_2.11/Main.java
@@ -0,0 +1,19 @@
+/*Задание 2,11: В переменной min лежит число от 0 до 59.
+        Определите в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).*/
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int min=23;
+        String s;
+        if(min>=0&&min<15) s="Первая четверть";
+        else if(min>=15&&min<30) s="Вторая четверть";
+        else if(min>=30&&min<45) s="Третья четверть";
+        else if(min>=45&&min<60) s="Четвертая четверть";
+        else s="Число за пределами диапазона 0-59";
+        System.out.println(s);
+
+    }
+}
