ea8ad3fa7a58607536c1b6f2b027c65cba9a0374
Helen <64192579+aloe99@users.noreply.github.com>
2020-04-27 22:51:37 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main  (String [] args) {
+    /*Задание 1.6 Напиши программу, которая выводит на экран квадрат переменной number;
+*/
+    int number = 50;
+    int pow = 2;
+    System.out.println(Math.pow(number, pow));
+}
+}
