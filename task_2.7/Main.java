8c7215685dbff36d871553711024dadd4efb33e8
Helen <64192579+aloe99@users.noreply.github.com>
2020-04-27 23:17:30 +0300

    Create Main.java

---
+++ task_2.7/Main.java
@@ -0,0 +1,12 @@
+/*Задание 2,7: Выведите в консоль столбец чисел от 1 до 50.*/
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=1;i<=50;i++)
+            System.out.println(i);
+
+    }
+}
