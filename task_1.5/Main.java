bab3df4146bcc94bcfa3ae82a329cca2b959816a
Helen <64192579+aloe99@users.noreply.github.com>
2020-04-27 22:40:03 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+/*Задание 1.5
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+/*
+public static void main  (String [] args) {
+  String name;
+  int age;
+  String city;
