eaab851183eaf070e526e009395b3150aa07c4ea
Helen <64192579+aloe99@users.noreply.github.com>
2020-04-28 08:54:42 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double v=sc.nextDouble();
-        double p=sc.nextDouble();
+        double vklad=scan.nextDouble();
+        double p=scan.nextDouble();
         for(int i=0;i<5;i++) {
-            v=v+v*p/100;
-            System.out.println(v);
+            vklad+=vklad*p/100;
+            System.out.println(vklad);
         }
 
     }
