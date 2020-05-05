630bb1821c9ecbc550a53ca45b95762f57e16810
Helen <64192579+aloe99@users.noreply.github.com>
2020-05-05 14:57:42 +0300

    Update Main.java

--- task_5.1/Main.java
+++ task_5.1/Main.java
@@ -1,16 +1,18 @@
-  
+package com.company;
 /* Помогите коту обрести имя с помощью метода setName. */
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.setName("Мурзик");
-        System.out.println(cat.Getname());
+        System.out.println(cat.getName());
     }
 }
 class Cat {
     private String name = "безымянный кот";
     public void setName(String name) {
-        this.name="Муся";
+        this.name=name;
+    }
+
     public String getName() {
         return this.name;
     }
