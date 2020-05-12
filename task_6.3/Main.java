22e765f4535bb1cf16046242159399315c7ff349
Helen <64192579+aloe99@users.noreply.github.com>
2020-05-12 10:32:07 +0300

    Create Main.java

---
+++ task_6.3/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+/*1. Создайте public static класс MarkTwainBook, который наследуется от Book. Имя автора [Mark Twain].
+Параметр конструктора - название книги (title).
+2. В классе MarkTwainBook реализуйте все абстрактные методы.
+3. Для метода getBook измените тип возвращаемого значения на более подходящий.
+4. Создайте по аналогии AgathaChristieBook. Имя автора [Agatha Christie].
+5. В классе Book реализуйте тело метода getOutputByBookType так, чтобы он возвращал:
+5.1. agathaChristieOutput для книг Агаты Кристи;
+5.2. markTwainOutput для книг Марка Твена.*/
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Book> books = new LinkedList<Book>();
+        books.add(new MarkTwainBook("Tom Sawyer"));
+        books.add(new AgathaChristieBook("Hercule Poirot"));
+        System.out.println(books);
+    }
+
+    abstract static class Book {
+        public String author;
+
+        public Book(String author) {
+            this.author = author;
+        }
+
+        public abstract Book getBook();
+
+        public abstract String getTitle();
+
+        private String getOutputByBookType() {
+            String agathaChristieOutput = author + ": " + getTitle() + " is a detective";
+            String markTwainOutput = getTitle() + " was written by " + author;
+            if(author.equals("Mark Twain")) return markTwainOutput;
+            else return agathaChristieOutput;
+        }
+
+        public String toString() {
+            return getOutputByBookType();
+        }
+    }
+}
